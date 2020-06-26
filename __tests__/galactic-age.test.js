40b6eb3a2e3de955958afd85269ca1179b39babc
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-26 10:33:11 -0700

    fail test for beforeEach of user.

---
+++ __tests__/galactic-age.test.js
@@ -0,0 +1,12 @@
+import { User } from './../src/galactic-age.js'
+
+describe('user', () => {
+
+  let reusableUser;
+
+  beforeEach(() => {
+    resuableUser = new User ();
+});
+  test('should show beforeEach working', () => {
+});
+});
