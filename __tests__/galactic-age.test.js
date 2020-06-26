59c121ace5ee301efaf1edb4fea2aee7ba8c2016
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-26 11:27:42 -0700

    pass test for beforeEach to establish working planet class test category.

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -6,13 +6,21 @@ describe('user', () => {
 
   beforeEach(() => {
     reusableUser = new User ();
+  });
+    test('should show beforeEach working for user class', () => {
+  });
+    test('should show User contain property for age', () => {
+      expect(reusableUser.age).toBe(30)
+  });
+    test('should show User contain property for life expectancy', () => {
+    expect(reusableUser.lifeEx).toBe(75)
+  });
 });
-  test('should show beforeEach working', () => {
-});
-  test('should show User contain property for age', () => {
-    expect(reusableUser.age).toBe(30)
-});
-  test('should show User contain property for life expectancy', () => {
-  expect(reusableUser.lifeEx).toBe(75)
-});
+describe('planet', () => {
+
+  beforeEach(() => {
+
+  });
+  test('should show beforeEach working for planet class', () => {
+  });
 });
