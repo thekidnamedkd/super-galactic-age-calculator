7044ac185ecccb375d65d94a68ac00daa299e0fd
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-10 15:54:00 -0700

    pass test for mercuryAge. fixed test from last commit.

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -2,13 +2,13 @@ export class User {
   constructor (age, lifeEx) {
     this.age = age;
     this.lifeEx = lifeEx;
+    this.mercuryAge = parseFloat(this.age / .24).toFixed(2);
   }
   
 // 'should calculate age divided by the constant of mercury' 
-calcMercuryAge() {
+// calcMercuryAge() {
   
-  }
-
+// }
 calcVenusAge() {
   let venusAge = parseFloat(this.age / .62).toFixed(2);
   return venusAge;
