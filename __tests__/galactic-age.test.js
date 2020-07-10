69291e664f2f345e61417565bfb2530d034d67c3
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-10 16:08:34 -0700

    fail test for 7 planetAge.

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -26,28 +26,28 @@ describe('planet age calculation', () => {
     reusableCalc = new User (30, 75);
   });
     test('should calculate age divided by the constant of mercury' , () => {
-    expect(reusableCalc.mercuryAge).toEqual("125.00");
+    expect(reusableCalc.mercuryAge).toBe("125.00");
   });
   test('should calculate age divided by the constant of mercury', () => {
-    expect(reusableCalc.calcVenusAge(30)).toContain(48.00);
+    expect(reusableCalc.venusAge).toBe("48.00");
   });
     test('should calculate age divided by the constant of earth', () => {
-    expect(reusableCalc.calcEarthAge(30)).toContain(30.00);
+    expect(reusableCalc.earthAge).toBe("30.00");
   });
     test('should calculate age divided by the constant of mars', () => {
-    expect(reusableCalc.calcMarsAge(30)).toContain(15.96);
+    expect(reusableCalc.marsAge).toBe("15.96");
   });
     test('should calculate age divided by the constant of jupiter', () => {
-    expect(reusableCalc.calcJupiterAge(30)).toContain(2.53);
+    expect(reusableCalc.jupiterAge).toBe("2.53");
   });
     test('should calculate age divided by the constant of saturn', () => {
-    expect(reusableCalc.calcSaturnAge(30)).toContain(1.02);
+    expect(reusableCalc.saturnAge).toBe("1.02");
   });
     test('should calculate age divided by the constant of uranus', () => {
-    expect(reusableCalc.calcUranusAge(30)).toContain(0.36);
+    expect(reusableCalc.uranusAge).toBe("0.36");
   });
     test('should calculate age divided by the constant of neptune', () => {
-    expect(reusableCalc.calcNeptuneAge(30)).toContain(0.18);
+    expect(reusableCalc.neptuneAge).toBe("0.18");
   });
 
 // describe('planet life expectancy', () => {
--- src/galactic-age.js
+++ src/galactic-age.js
@@ -4,42 +4,7 @@ export class User {
     this.lifeEx = lifeEx;
     this.mercuryAge = parseFloat(this.age / .24).toFixed(2);
   }
-  
-// 'should calculate age divided by the constant of mercury' 
-// calcMercuryAge() {
-  
-// }
-calcVenusAge() {
-  let venusAge = parseFloat(this.age / .62).toFixed(2);
-  return venusAge;
-  }
-calcEarthAge() {
-  let earthAge = parseFloat(this.age / 1).toFixed(2)
-  return earthAge;
-  }
-calcMarsAge() {
-  let marsAge = parseFloat(this.age / 1.88).toFixed(2)
-  return marsAge;
-  }
-
-calcJupiterAge() {
-  let jupiterAge = parseFloat(this.age / 11.86).toFixed(2)
-  return jupiterAge;
-  }
-calcSaturnAge() {
-  let saturnAge = parseFloat(this.age / 29.45).toFixed(2)
-  return saturnAge;
-  }
-calcUranusAge() {
-  let uranusAge = parseFloat(this.age / 84.45).toFixed(2)
-  return uranusAge;
-  }
-calcNeptuneAge () {
-  let neptuneAge = parseFloat(this.age / 164.8).toFixed(2)
-  return neptuneAge;
-  }
-
-calcLifeExpect () {
-  
-  }
 }
+// calcLifeExpect () {
+//   }
+// }
