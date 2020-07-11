0817096db235568ff05becfde94dece1cbc03a8f
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-11 00:07:57 -0700

    add functionality for positive returns to display years left to on each planet.

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import { User } from './galactic-age.js'
 function outputs () {
   let newUser = new User($("#age").val(), $("#lifeEx").val());
   for (let i=0; i<newUser.agesReturned.length; i++) {
-    $("#ageList").append(`<li>Your age in ${newUser.planetName[i]} years is: ${newUser.agesReturned[i]}</li>`)
+    $("#ageList").append(`<li>Your age in ${newUser.planetName[i]} years is ${newUser.agesReturned[i]}.</li>`)
   }
   for (let i=0; i<newUser.exesReturned.length; i++) {
-    if (newUser.exesReturned[i] < 0 ) {
-    $("#exesList").html(`<li>On ${newUser.planetName[i]} have outlived your life expectancy on Earth by: ${(-1 * newUser.exesReturned[i])}</li>`)
+    if (newUser.exesReturned[i] < 0) {
+    $("#exesList").append(`<li>On ${newUser.planetName[i]} have outlived your life expectancy on Earth by ${(-1 * newUser.exesReturned[i])} years.</li>`)
+    } else if (newUser.exesReturned[i] > 0) {
+      $("#exesList").append(`<li>You have ${newUser.exesReturned[i]} Earth years left to live on ${newUser.planetName[i]}.</li>`)
     }
   }
 }
